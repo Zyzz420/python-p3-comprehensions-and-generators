8eb2426c564d7eec323dcadb8ed6c2291554c04e
Zyzz king <abdulaziz.komara@student.moringaschool.com>
2023-08-18 10:44:38 -0400

    Assignment complete

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
-
 def return_evens(num_list):
-    pass
+    result = []
+    for i in num_list:
+        if i % 2 == 0:
+            result.append(i)
+    return result
 
-def make_exclamation(sentence_list):
-    pass
+print(return_evens([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
+        
+
+            
+
+def make_exclamation(sentences):
+    return [sentence + "!" for sentence in sentences]
